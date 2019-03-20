1475d3cde88f6975bf6a5e442983443e341718c0
milokia <kiaeedar@ualberta.ca>
2019-03-19 21:41:20 -0600

    some comments

--- trans_coord.py
+++ trans_coord.py
@@ -34,28 +34,48 @@ obbTree.Update()
 pts = polydata.GetPoints()
 
 # and vectors of local coordinate system in terms of the original one
+
 xprime = np.array(pts.GetPoint(1)) - np.array(pts.GetPoint(0))
+xprime = xprime/np.linalg.norm(xprime)
+print("xprime: ", xprime)
+
 yprime = np.array(pts.GetPoint(2)) - np.array(pts.GetPoint(0))
+yprime = yprime/np.linalg.norm(yprime)
+print("yprime: ", yprime)
+
 zprime = np.array(pts.GetPoint(4)) - np.array(pts.GetPoint(0))
+zprime = zprime/np.linalg.norm(zprime)
+print("zprime: ", zprime)
+
+# corner coordinates
 oprime = np.array(pts.GetPoint(0))
+print("oprime: ", oprime)
+
+# we need to find some origin for the geometry
+com = vtk.vtkCenterOfMass()
+com.SetInputData(pd)
+com.SetUseScalarsAsWeights(False)
+com.Update()
+center = [0, 0, 0]
+center = com.GetCenter()
+
 
 # define the rotation matrix, here it is simply the local coordinate system
 rotM = vtk.vtkMatrix4x4()
 rotM.Identity()
 
 for i in range(0, 3):
-    rotM.SetElement(i, 0, xprime[i])
+    rotM.SetElement(0, i, xprime[i])
+    rotM.SetElement(1, i, yprime[i])
+    rotM.SetElement(2, i, zprime[i])
 
-for i in range(0, 3):
-    rotM.SetElement(i, 1, yprime[i])
-
-for i in range(0, 3):
-    rotM.SetElement(i, 2, zprime[i])
+#rotM.Invert()
 
 # transform object stores the transformation information
 trans = vtk.vtkTransform()
-trans.Translate(-1*oprime[0], -1*oprime[1], -1*oprime[2])
-trans.SetMatrix(rotM)
+trans.Translate(-center[0], -center[1], -center[2])
+trans.PostMultiply()
+trans.Concatenate(rotM)
 
 # transform filter performs the actual operation
 transF = vtk.vtkTransformPolyDataFilter()
@@ -63,10 +83,29 @@ transF.SetInputData(pd)
 transF.SetTransform(trans)
 transF.Update()
 
+pd = transF.GetOutput()
+
+
+pd = transF.GetOutput()
+b = pd.GetBounds()
+z_min = b[4]
+
+# transform object stores the transformation information
+trans2 = vtk.vtkTransform()
+trans2.Translate(0, 0, -z_min)
+
+# transform filter performs the actual operation
+transF2 = vtk.vtkTransformPolyDataFilter()
+transF2.SetInputData(pd)
+transF2.SetTransform(trans2)
+transF2.Update()
+
+pd = transF2.GetOutput()
+
 # writing the output
-writer = vtk.vtkPolyWriter()
-writer.SetFileName(args.input)
-writer.SetInputData(transF.GetOutput())
+writer = vtk.vtkPolyDataWriter()
+writer.SetFileName(args.output)
+writer.SetInputData(pd)
 writer.Update()
 writer.Write()
 
